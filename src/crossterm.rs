b32350570e5307a586eb91e8a4eddf32602f45e1
theaaaaaaa <theah0032@gmail.com>
2021-09-28 11:00:57 -0500

    more docs and features

--- src/crossterm.rs
+++ src/crossterm.rs
@@ -2,9 +2,15 @@ use std::io::Stdout;
 
 use crate::out::{Action, Handler};
 
-use crossterm::{cursor::MoveTo, queue, style::Print};
+use crossterm::{cursor::MoveTo, execute, queue, style::Print};
 /// A basic wrapper for crossterm. Turns this output into crossterm-based output. 
 pub struct CrosstermHandler;
+impl CrosstermHandler {
+    /// Flushes any stray text into the terminal. 
+    pub fn finish(out: &mut Stdout) -> Result<(), crossterm::ErrorKind> {
+        execute!(out)
+    }
+}
 
 impl Handler for CrosstermHandler {
     type OutputDevice = Stdout;
